f8f31c49d46e0bcd0c0f4f9574f6ee96a9fc1d4f
Hammuda007 <sheakho19@gmail.com>
2022-12-29 02:15:24 +0100

    upload image

--- posts/models.py
+++ posts/models.py
@@ -7,3 +7,9 @@ from django.db import models
 class Post(models.Model):
     title=models.CharField(max_length=100)
     content=models.TextField(max_length=10000)
+    image=models.ImageField(upload_to='posts/')
+
+
+
+    def __str__(self):
+        return self.title
