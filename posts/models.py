fad58ddb9f6cddeeb94b7122250f0c2763e39910
Hammuda007 <sheakho19@gmail.com>
2023-01-05 10:02:22 +0100

    add new topic

--- posts/models.py
+++ posts/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from taggit.managers import TaggableManager
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -8,6 +10,9 @@ class Post(models.Model):
     title=models.CharField(max_length=100)
     content=models.TextField(max_length=10000)
     image=models.ImageField(upload_to='posts/')
+    author=models.ForeignKey(User,related_name='post_author',on_delete=models.CASCADE)
+
+    tags = TaggableManager()
 
 
 
